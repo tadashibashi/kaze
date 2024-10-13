c736284e3ce2048da9a4c4a8685149604d06c368
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-10-13 13:56:16 -0700

    Add AssetLoader class and tests

---
+++ kaze/core/AssetLoader.h
@@ -0,0 +1,149 @@
+#pragma once
+#ifndef kaze_core_assetloader_h_
+#define kaze_core_assetloader_h_
+
+#include <kaze/kaze.h>
+#include <kaze/concepts.h>
+
+#include <mutex>
+#include <kaze/debug.h>
+
+KAZE_NAMESPACE_BEGIN
+
+template <Hashable K, LoadableAsset<K> T>
+class AssetLoader {
+public:
+    AssetLoader() : m_assets(), m_paths(), m_lock() { }
+    ~AssetLoader() { clear(); }
+
+
+    /// Load an asset from a file on disk, or pre-existing asset in cache.
+    /// @param[in]  key   path to the file
+    /// @returns pointer to the asset or nullptr on error.
+    auto load(const K &key) -> const T *
+    {
+        std::lock_guard lockGuard(m_lock);
+
+        auto it = m_assets.find(key);
+        if (it == m_assets.end())
+        {
+            // load new asset
+            auto result = m_assets.emplace(key, T{});
+            const auto &assetKey = result.first->first;
+            auto &      asset     = result.first->second;
+
+            if ( !asset.load(assetKey) )
+            {
+                m_assets.erase(assetKey);
+                return nullptr;
+            }
+
+            m_paths[&asset] = assetKey.c_str();
+            return &asset;
+        }
+
+        // get cached asset
+        return &it->second;
+    }
+
+
+    /// Check if the asset loader contains an asset
+    /// @param[in] key  asset path to check
+    /// @returns whether asset with `filepath` exists in this container
+    auto contains(const K &key) -> Bool
+    {
+        return m_assets.contains(key);
+    }
+
+
+    /// Check if the asset loader contains an asset
+    /// @param[in] asset  asset to check
+    /// @returns whether asset exists in this container
+    auto contains(const T *asset) -> Bool
+    {
+        return m_paths.contains(asset);
+    }
+
+
+    /// Unload an asset by filepath
+    /// @param[in]  key  - path of asset to unload
+    /// @returns `true`  - asset was released and removed from this loader;
+    ///          `false` - asset with associated `filepath` does not belong to this loader
+    auto unload(const K &key) -> Bool
+    {
+        std::lock_guard lockGuard(m_lock);
+
+        auto it = m_assets.find(key);
+        if (it != m_assets.end())
+        {
+            it->second.release();
+            m_paths.erase(&it->second);
+            m_assets.erase(it);
+
+            return KAZE_TRUE;
+        }
+
+        return KAZE_FALSE;
+    }
+
+
+    /// Unload an asset by pointer.
+    /// @param[in]  asset   pointer to asset to unload
+    /// @returns `true`  - asset was released and removed from this loader;
+    ///          `false` - asset does not belong to this loader and is not mutated
+    auto unload(const T *asset) -> Bool
+    {
+        std::lock_guard lockGuard(m_lock);
+
+        auto it = m_paths.find(asset);
+        if (it != m_paths.end())
+        {
+            auto assetIt = m_assets.find(it->second);
+            if (assetIt == m_assets.end())
+            {
+                KAZE_CORE_ERRCODE(Error::LogicErr, "Internal error, failed to find matching asset in cache: {}",
+                    it->second);
+                return KAZE_FALSE;
+            }
+
+            assetIt->second.release();
+            m_assets.erase(it->second);
+            m_paths.erase(it);
+            return KAZE_TRUE;
+        }
+
+        return KAZE_FALSE;
+    }
+
+
+    /// Unload all assets.
+    auto clear() -> void
+    {
+        std::lock_guard lockGuard(m_lock);
+        for (auto &[filepath, asset] : m_assets)
+        {
+            asset.release();
+        }
+
+        m_assets.clear();
+        m_paths.clear();
+    }
+
+
+    /// Get the number of assets currently loaded in this container.
+    [[nodiscard]]
+    auto size() const noexcept -> Size { return m_assets.size(); }
+
+
+    /// Check whether any assets are currently loaded in this container.
+    [[nodiscard]]
+    auto empty() const noexcept -> Bool { return m_assets.empty(); }
+private:
+    Map<K, T> m_assets;
+    Dictionary<const T *, K> m_paths;
+    std::mutex m_lock;
+};
+
+KAZE_NAMESPACE_END
+
+#endif
---
+++ tests/unit_tests/kaze/core/AssetLoader.test.cpp
@@ -0,0 +1,144 @@
+#include <doctest/doctest.h>
+#include <kaze/core/AssetLoader.h>
+
+USING_KAZE_NAMESPACE;
+
+/// Fulfills AssetLoadable concept
+/// - It must have a `load` function that returns a `Bool` and accepts a `const K &` to perform the load with
+/// - K must be hashable
+/// - It must have a `release` function that takes no parameters and returns `void`
+struct TestAsset
+{
+    TestAsset() : m_isLoaded(KAZE_FALSE), m_path() { }
+    ~TestAsset()
+    {
+        m_isLoaded = false;
+    }
+
+    auto load(const String &filepath) -> Bool
+    {
+        m_isLoaded = true;
+        m_path = filepath;
+        ++s_aliveCount;
+        return KAZE_TRUE;
+    }
+
+    auto release() -> void
+    {
+        m_isLoaded = false;
+        m_path = "";
+        --s_aliveCount;
+    }
+
+    [[nodiscard]]
+    static Int aliveCount() { return s_aliveCount; } // checking ensures loadFile and release are called
+    static void resetAliveCount() { s_aliveCount = 0; }
+private:
+    Bool m_isLoaded;
+    String m_path;
+
+    static Int s_aliveCount;
+};
+
+Int TestAsset::s_aliveCount{};
+
+TEST_SUITE("AssetLoader")
+{
+    TEST_CASE("initialization")
+    {
+        AssetLoader<String, TestAsset> assets{};
+        CHECK(assets.empty());
+        CHECK(assets.size() == 0);
+        CHECK(TestAsset::aliveCount() == 0);
+    }
+
+    TEST_CASE("load assets")
+    {
+        AssetLoader<String, TestAsset> assets{};
+        auto asset0 = assets.load("abcdefg");
+        // checking alive count ensures number of calls to TestAsset::loadFile / TestAsset::release
+        CHECK(TestAsset::aliveCount() == 1);
+        auto asset1 = assets.load("hijklmn");
+        CHECK(TestAsset::aliveCount() == 2);
+        auto asset2 = assets.load("opqrstu");
+        CHECK(TestAsset::aliveCount() == 3);
+        auto asset3 = assets.load("vwxyz");
+        CHECK(TestAsset::aliveCount() == 4);
+
+        CHECK(asset0 != nullptr);
+        CHECK(asset1 != nullptr);
+        CHECK(asset2 != nullptr);
+        CHECK(asset3 != nullptr);
+        CHECK(assets.size() == 4);
+        CHECK( !assets.empty() );
+    }
+
+    TEST_CASE("load/unload asset by path")
+    {
+        AssetLoader<String, TestAsset> assets{};
+        auto asset0 = assets.load("abcdefg");
+        auto asset1 = assets.load("hijklmn");
+        auto asset2 = assets.load("opqrstu");
+        auto asset3 = assets.load("vwxyz");
+
+        CHECK(asset0 != nullptr);
+        CHECK(asset1 != nullptr);
+        CHECK(asset2 != nullptr);
+        CHECK(asset3 != nullptr);
+        CHECK(assets.size() == 4);
+        CHECK( !assets.empty() );
+
+        CHECK(assets.unload("abcdefg"));
+        CHECK(TestAsset::aliveCount() == 3);
+        CHECK(assets.size() == 3);
+        CHECK(assets.unload("hijklmn"));
+        CHECK(TestAsset::aliveCount() == 2);
+        CHECK(assets.size() == 2);
+        CHECK(assets.unload("opqrstu"));
+        CHECK(TestAsset::aliveCount() == 1);
+        CHECK(assets.size() == 1);
+        CHECK(assets.unload("vwxyz"));
+        CHECK(TestAsset::aliveCount() == 0);
+        CHECK(assets.size() == 0);
+    }
+
+    TEST_CASE("unload by pointer")
+    {
+        AssetLoader<String, TestAsset> assets{};
+        auto asset0 = assets.load("abcdefg");
+        auto asset1 = assets.load("hijklmn");
+        auto asset2 = assets.load("opqrstu");
+        auto asset3 = assets.load("vwxyz");
+
+        CHECK(assets.unload(asset0));
+        CHECK( !assets.contains(asset0) );
+        CHECK(assets.size() == 3);
+
+        CHECK(assets.unload(asset1));
+        CHECK( !assets.contains(asset1) );
+        CHECK(assets.size() == 2);
+
+        CHECK(assets.unload(asset2));
+        CHECK( !assets.contains(asset2) );
+        CHECK(assets.size() == 1);
+
+        CHECK(assets.unload(asset3));
+        CHECK( !assets.contains(asset3) );
+        CHECK(assets.size() == 0);
+    }
+
+    TEST_CASE("clear all")
+    {
+        AssetLoader<String, TestAsset> assets{};
+        auto asset0 = assets.load("abcdefg");
+        auto asset1 = assets.load("hijklmn");
+        auto asset2 = assets.load("opqrstu");
+        auto asset3 = assets.load("vwxyz");
+        CHECK(assets.size() == 4);
+        CHECK(TestAsset::aliveCount() == 4);
+
+        assets.clear();
+        CHECK(assets.empty());
+        CHECK(TestAsset::aliveCount() == 0);
+    }
+}
